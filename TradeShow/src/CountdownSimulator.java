f299b7118adae92935b17a2a8b3485691adac3f4
Ryan <rmf2490@gmail.com>
2012-01-21 09:02:25 -0500

    TradeShowDriver should now work as intended.
    Added test code to CDS's run() method. This is not how the cde really works, since it will need to call the SubjectDelegate.

--- TradeShow/src/CountdownSimulator.java
+++ TradeShow/src/CountdownSimulator.java
@@ -13,7 +13,7 @@ public class CountdownSimulator implements interfaces.Runnable,
 		CountdownSubject {
 	private String movieTime;
 	private String movieName;
-	private boolean isRealTime;
+	private boolean realTime;
 	private SubjectDelegate SD;
 	ArrayList<CountdownObserver> Observers;
 
@@ -30,12 +30,16 @@ public class CountdownSimulator implements interfaces.Runnable,
 	public CountdownSimulator(String movie, String time, boolean isRealTime) {
 		movieName = movie;
 		movieTime = time;
+		realTime = isRealTime;
 		Observers = new ArrayList<CountdownObserver>();
 
 	}// constructor
 
 	public void run() {
-
+		//This is just test code, the code needs to be updated to work as required
+		while(Integer.parseInt(movieTime) > 0){
+			System.out.println(movieName + "/Time left: " + notifyObservers(movieTime));
+		}
 	}// run method
 
 	public void addObserver(CountdownObserver observer) {
--- TradeShow/src/TradeShowDriver.java
+++ TradeShow/src/TradeShowDriver.java
@@ -1,12 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class TradeShowDriver {
+public class TradeShowDriver implements Runnable{
 
-	CountdownDisplay display;
+	private CountdownDisplay display;
+	private CountdownSimulator sim;
+	private Thread controlThread;
 
 	public static void main(String[] args) throws IOException {
-
+		CountdownSimulator FightClub = new CountdownSimulator("Fight Club", "139", true);
+		CountdownSimulator CitizenKane = new CountdownSimulator("Citizen Kane", "119", true);
+		CountdownSimulator PulpFiction = new CountdownSimulator("Pulp Fiction", "154", true);
+		CountdownSimulator Avatar = new CountdownSimulator("James Cameron's Avatar", "162", true);
+		CountdownSimulator ToyStoryThree = new CountdownSimulator("Toy Story 3", "102", true);
+		CountdownDisplay Printer = new CountdownDisplay();
+		
+		new TradeShowDriver(FightClub, Printer).start();
+		new TradeShowDriver(CitizenKane, Printer).start();
+		new TradeShowDriver(PulpFiction, Printer).start();
+		new TradeShowDriver(Avatar, Printer).start();
+		new TradeShowDriver(ToyStoryThree, Printer).start();
+		
+		//System.out.println("Done creating simulator objects");
+	}
+	public TradeShowDriver(CountdownSimulator sim, CountdownDisplay display){
+		this.sim = sim;
+		this.display = display;
+		controlThread = new Thread(this);
+	}
+	public void run(){
+		sim.addObserver(display);
+		sim.run();
+	}
+	public void start(){
+		controlThread.start();
 	}
 
 }// end class
