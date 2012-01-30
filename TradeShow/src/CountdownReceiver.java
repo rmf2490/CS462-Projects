315ca1bda2d73745d540aa099453aec50809ebe3
Ryan <rmf2490@gmail.com>
2012-01-30 16:46:07 -0500

    Added CountdownSender and CountdownReceiver.

---
+++ TradeShow/src/CountdownReceiver.java
@@ -0,0 +1,25 @@
+import interfaces.CountdownObserver;
+import interfaces.CountdownSubject;
+
+
+public class CountdownReceiver implements CountdownSubject {
+
+	@Override
+	public void addObserver(CountdownObserver observer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyObservers(String time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeObservers(CountdownObserver observer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TradeShow/src/CountdownSender.java
@@ -0,0 +1,12 @@
+import interfaces.CountdownObserver;
+
+
+public class CountdownSender implements CountdownObserver {
+
+	@Override
+	public void handleTime(String time) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
